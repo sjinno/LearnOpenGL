c0cf8a53548a0eb14656666f9d8ce5a80d63bcf0
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-10 17:06:51 -0700

    Create a colorful triangle.

--- examples/shaders/main.rs
+++ examples/shaders/main.rs
@@ -1,7 +1,9 @@
+mod colorful_triangle;
 mod colorize_triangle;
 mod move_triangle;
 
 fn main() {
     // move_triangle::move_triangle();
-    colorize_triangle::colorize_triangle();
+    // colorize_triangle::colorize_triangle();
+    colorful_triangle::colorful_triangle();
 }
